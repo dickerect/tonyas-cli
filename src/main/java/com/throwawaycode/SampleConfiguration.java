95fe7f2736600dc1d6d1960ddd8ade07ffcb33b6
propspero238 <keithc238@gmail.com>
2016-04-05 13:28:14 -0700

    added demonstration of importing existing spring configs

--- src/main/java/com/throwawaycode/SampleConfiguration.java
+++ src/main/java/com/throwawaycode/SampleConfiguration.java
@@ -1,8 +1,17 @@
 package com.throwawaycode;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SampleConfiguration
-{
+public class SampleConfiguration {
+
+    @Bean
+    public Map forDemonstratingImportOfSpringConfigClass() {
+        return new HashMap<>();
+    }
+
 }
